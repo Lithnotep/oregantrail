c17f5e1cc02a6b57fb7ccbba70cb97cf54cc6a5b
Max Mitrani <mjmitrani168@gmail.com>
2020-07-28 10:13:00 -0600

    add end of practice

--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -1,10 +1,12 @@
 require 'minitest/autorun'
+require 'pry'
 require './lib/linked_list'
 require "./lib/node"
 class LinkedListTest < Minitest::Test
   def test_linked_list
     @list = LinkedList.new
-    assert_nil @list.head
+
+    assert @list.head = false
     @list.append("Cohan")
     assert @list.head = "Cohan"
     assert @list.to_string = "The Cohan Family"
